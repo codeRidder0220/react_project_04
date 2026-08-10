918f183e856b19e2d8e625ba2d4d07facaccad49
Karthik <theankitsingh2002@gmail.com>
2026-08-10 19:24:34 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import Step1 from "./components/Step1"
+import Step2 from "./components/Step2"
+import Step3 from "./components/Step3"
 
 function App() {
 
   return (
-    <>
-    
-    </>
+    <div className="min-h-screen bg-linear-[135deg] from-gray-900 via-blue-900 to-purple-900 ">
+
+    <Step1/>
+    <Step2/>
+    <Step3/>
+
+    </div>
   )
 }
 
